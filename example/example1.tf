d48d7153c4e9e9d6f822d72eb4e719d882a192de
Emile Hofsink <emile.hofsink@withriley.com>
2022-02-11 14:14:24 +1100

    example updated to include remote path

--- example/example1.tf
+++ example/example1.tf
@@ -1,5 +1,5 @@
 module "cloudhealth-terraform" {
-    source = "../"
+    source = "github.com/withriley/terraform-cloudhealth"
     billing_account_id        = "000000-000000-000000"
     org_id                    = "000000000000"
     billing_export_project_id = "example-billing-project"
